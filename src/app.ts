d653c0a2c622b12f5dc07d3c2b37068e404b2f62
𝖲𝖨𝖥 𝖥𝖠𝖧𝖨𝖬 <siffahim25@gmail.com>
2023-06-06 17:29:02 +0600

    husky testing

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,14 @@
-import cors from "cors";
-import express, { Application, Request, Response } from "express";
-const app: Application = express();
-const port = 5000;
-
+import cors from 'cors'
+import express, { Application, Request } from 'express'
+const app: Application = express()
 
 //middleware and parser
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-
-app.get("/", async (req: Request, res: Response) => {
-    res.send("Hello World")
+app.get('/', async (req: Request, res: any) => {
+  res.send('Hello World')
 })
 
-export default app;
+export default app
