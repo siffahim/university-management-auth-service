fcb2e190f2b2736f8536efe6c35d75834e6530a8
𝖲𝖨𝖥 𝖥𝖠𝖧𝖨𝖬 <siffahim25@gmail.com>
2023-06-06 17:56:29 +0600

    without lint and prettier check

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,9 @@
 import cors from 'cors'
-import express, { Application, Request } from 'express'
+import express, { Application, Request, Response } from 'express'
 const app: Application = express()
 
 //middleware and parser
-app.use(cors())
+                app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
